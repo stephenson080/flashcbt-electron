b161826aa46f178056c89c3b2f374cd2ccf43388
Stephen Udeh <65906366+stephenson080@users.noreply.github.com>
2022-05-08 20:46:56 +0100

    Delete etherum-projects directory

--- etherum-projects/bank-app/client-side/ethereum/bankInstance.js
+++
@@ -1,6 +0,0 @@
-import ABI from '../../artifacts/contracts/bank.sol/Bank.json'
-import web3 from './web3-config'
-
-const bankContract = new web3.eth.Contract(ABI.abi, '0x73D4E8391291184C346d8BCeC569408530f18380')
-
-export default bankContract
--- etherum-projects/bit-fund/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import 'semantic-ui-css/semantic.min.css'
-import { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
